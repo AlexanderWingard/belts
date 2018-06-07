561cee1a30d7e417ecc115acf5c83281d0081f30
Alexander Wingård <alexander.wingard@gmail.com>
2018-06-07 23:38:34 +0200

    Start sketching on rpc-able components

--- test/factorio/core_test.clj
+++ test/factorio/core_test.clj
@@ -1,6 +1,7 @@
 (ns factorio.core-test
   (:require [clojure.test :refer :all]
-            [clojure.core.async :refer [pipe chan <! >! >!! timeout alts!! go-loop go]]
+            [clojure.core.match :refer [match]]
+            [clojure.core.async :refer [pipe chan <!! <! >! >!! timeout alts!! go-loop go]]
             [factorio.core :refer :all]))
 
 (defn plus-one [{:keys [n]}]
@@ -12,18 +13,25 @@
 (defn state-component [{:keys [state]}]
   {:n (swap! state inc)})
 
+(defn rpc-able [{:keys [state] :as msg}]
+  (match [msg]
+         [{:rpc "get" :from from}] (>!! from @state)
+         [{:rpc "set" :from from :val v}] (>!! from (reset! state v)))
+  nil)
+
 (defn component [meat & [cfg]]
   (let [in (chan)
         out (chan)]
     (go-loop []
       (when-let [msg (<! in)]
-        (>! out (meat (merge cfg msg)))
+        (when-let [return (meat (merge cfg msg))]
+          (>! out return))
         (recur)))
     {:in in :out out}))
 
 (defn read-with-timeout [c]
   (let [out (:out c)
-        [value channel] (alts!! [out (timeout 1000)])]
+        [value channel] (alts!! [out (timeout 10)])]
     (if (= channel out)
       value
       (throw (AssertionError. "Timeout")))))
@@ -34,6 +42,12 @@
 (defn connect [c1 c2]
   (pipe (:out c1) (:in c2)))
 
+(defn rpc [c rpc & [extra]]
+  (let [self (chan)
+        msg (merge {:rpc rpc :from self} extra)
+        _ (give-component c msg)]
+    (<!! self)))
+
 (deftest commponent-test
   (testing "components"
     (let [p (component plus-one)
@@ -42,11 +56,22 @@
           _ (connect p t)
           multed (read-with-timeout t)]
       (is (= {:n 10} multed))))
+
   (testing "stateful component"
-    (let [c (component state-component {:state (atom 0)})
-          _ (give-component c {})
+    (let [c  (component state-component {:state (atom 0)})
+          _  (give-component c {})
           s1 (read-with-timeout c)
-          _ (give-component c {})
+          _  (give-component c {})
           s2 (read-with-timeout c)]
       (is (= {:n 1} s1))
-      (is (= {:n 2} s2)))))
+      (is (= {:n 2} s2))))
+
+  (testing "rpc-able component"
+    (let [self (chan)
+          c  (component rpc-able {:state (atom "red")})
+          v1 (rpc c "get")
+          _  (rpc c "set" {:val "blue"})
+          v2 (rpc c "get")]
+      (is (= "red" v1))
+      (is (= "blue" v2))
+      (is (thrown? AssertionError (read-with-timeout c))))))
