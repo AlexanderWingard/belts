bdd21c51d459855ffaccc4429520d33e1cb48952
Alexander Wingård <alexander.wingard@gmail.com>
2018-08-03 04:07:14 +0200

    tmp

--- src/belts/core.clj
+++ src/belts/core.clj
@@ -10,6 +10,8 @@
   [x]
   (satisfies? clojure.core.async/Mult x))
 
+(defn uuid [] (str (java.util.UUID/randomUUID)))
+
 (s/def ::chan channel?)
 (s/def ::mult mult?)
 (s/def ::port (s/or :chan ::chan
@@ -22,29 +24,81 @@
                        :chan ::chan
                        :nil nil?))
 
+(defonce nodes (atom {}))
+(def nodes-dist (chan))
+(def nodes-mult (mult nodes-dist))
+
+(add-watch nodes :node-watcher (fn [k a o-s n-s]
+                                 (go (>! nodes-dist {:inspector
+                                                     (map #(select-keys (second %1) [:id :state :messages :source :target]) n-s)}))))
+
+(def nodes-compo {:in nodes-dist :out nodes-mult})
+
+(defn blinker [id]
+  (let [c (chan)]
+    (go-loop [in c t-o (chan)]
+      (let [[msg from] (alts! [in t-o])]
+        (cond
+          (= "active" msg)
+          (do
+            (swap! nodes assoc-in [id :state] "active")
+            (recur in (chan)))
+
+          (= "idle" msg)
+          (do
+            (recur in (timeout 3000)))
+
+          (= from t-o)
+          (do
+            (println id "idle")
+            (swap! nodes assoc-in [id :state] "idle")
+            (recur in (chan))))))
+    c))
+
+
+(defn add-node [data]
+  (swap! nodes update (:id data) merge data))
+
+(defn remove-node [id]
+  (swap! nodes dissoc id))
+
+(defn inc-msg [id]
+  (swap! nodes update-in [id :messages] inc))
+
+(defn change-state [id state]
+  (when-some [chan (get-in @nodes [id :blinker])]
+    (go (>! chan state))))
+
 (defn validate [spec val & extra]
   (if-not (s/valid? spec val)
     (throw (AssertionError. (str (s/explain-str spec val) extra)))))
 
 (defn component [meat & [cfg]]
   (let [in (chan)
-        out (chan)]
+        out (chan)
+        id (uuid)]
     (go-loop []
       (if-let [msg (<! in)]
         (do
           (validate ::msg-in msg meat)
+          (change-state id "active")
+          (inc-msg id)
           (let [msg-wo-from (dissoc msg ::from)
-                return (if (some? cfg)
-                         (meat msg-wo-from cfg)
-                         (meat msg-wo-from))
+                return (meat msg-wo-from)
                 return-to (get msg ::from out)]
             (validate ::msg-out return meat)
             (cond
               (channel? return) (pipe return return-to false)
               (some? return) (>! return-to return)))
+          (change-state id "idle")
           (recur))
-        (close! out)))
-    {:in in :out out}))
+        (do
+          (when (:pass-close cfg)
+            (meat nil))
+          (close! out)
+          (remove-node id))))
+    (add-node {:id id :info (str meat) :messages 0 :blinker (blinker id)})
+    {:id id :in in :out out}))
 
 (defn shutdown [{:keys [in out] :as c}]
   (close! in)
@@ -70,13 +124,27 @@
 (defn give-coll-to-component [compo coll]
   (<!! (onto-chan (:in compo) coll false)))
 
+(defn bpipe [from to]
+  (let [in (chan)
+        id (uuid)]
+    ((if (mult? (:out from)) tap pipe) (:out from) in true)
+    (add-node {:id id :source (:id from) :target (:id to) :messages -1})
+    (go-loop []
+      (if-let [msg (<! in)]
+        (do
+          (>! (:in to) msg)
+          (recur))
+        (do
+          (remove-node id)
+          (close! (:in to)))))))
+
 (defn graph [g]
   (doseq [thread g
           [from to] (partition 2 1 thread)]
     (validate ::component from)
     (validate ::component to)
-    ((if (mult? (:out from)) tap pipe) (:out from) (:in to) true))
-  {:in (:in (first (first g))) :out (:out (last (last g)))})
+    (bpipe from to))
+  {:id (uuid) :in (:in (first (first g))) :out (:out (last (last g)))})
 
 (defn dead-end [{:keys [out] :as c}]
   (close! out)
@@ -87,15 +155,17 @@
 
 (defn cloner []
   (let [in (chan)
-        m (mult in)]
-    {:in in :out m}))
+        m (mult in)
+        id (uuid)]
+    {:id id :in in :out m}))
 
 (defn echo []
   (component identity))
 
 (defn debouncer [interval]
   (let [in (chan (sliding-buffer 1))
-        out (chan)]
+        out (chan)
+        id (uuid)]
     (go-loop []
       (if-let [msg (<! in)]
         (do
@@ -103,11 +173,12 @@
           (<! (timeout interval))
           (recur))
         (close! out)))
-    {:in in :out out}))
+    {:id id :in in :out out}))
 
 (defn stabilizer [interval]
   (let [in (chan (sliding-buffer 1))
-        out (chan)]
+        out (chan)
+        id (uuid)]
     (go-loop [msg (<! in)]
       (if (some? msg)
         (do
@@ -118,15 +189,16 @@
               (>! out msg)
               (recur (<! in)))))
         (close! out)))
-    {:in in :out out}))
+    {:id id :in in :out out}))
 
 (defn slider []
   (let [c (chan (sliding-buffer 1))]
-    {:in c :out c}))
+    {:id (uuid) :in c :out c}))
 
 (defn ticker [interval]
   (let [in (chan)
-        out (chan)]
+        out (chan)
+        id (uuid)]
     (go-loop [n 0]
       (>! out {:tick n})
       (let [t-o (timeout interval)
@@ -134,7 +206,7 @@
         (if (= from t-o)
           (recur (inc n))
           (close! out))))
-    {:in in :out out}))
+    {:id id :in in :out out}))
 
 (defn rpc [compo args]
   (let [self (chan)
@@ -147,12 +219,15 @@
     (onto-chan out coll)
     out))
 
-(defn fun-cache-meat [msg {:keys [compo cache]}]
-  (if (= msg (:msg @cache))
-    (:res @cache)
-    (let [res (rpc compo msg)]
-      (reset! cache {:msg msg :res res})
-      res)))
-
-(defn fun-cache [c]
-  (component fun-cache-meat {:compo c :cache (atom {})}))
+(defn fun-cache [compo]
+  (component
+   (let [cache (atom {})]
+     (fn [msg]
+       (if-some [m msg]
+         (if (= msg (:msg @cache))
+           (:res @cache)
+           (let [res (rpc compo msg)]
+             (reset! cache {:msg msg :res res})
+             res))
+         (shutdown compo))))
+   {:pass-close true}))
